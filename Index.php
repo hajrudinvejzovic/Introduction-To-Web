2d27b17d5e0c74a6618be40a8ba341ec499dcce9
hajrudinvejzovic <hajrudin.vejzovic@stu.ibu.edu.ba>
2022-03-15 19:32:04 +0100

    new refresh

--- Index.php
+++ Index.php
@@ -1,9 +1,25 @@
 <?php
-require "vendor/autoload.php";
 
-Flight::route("/", function(){
-  echo "Ja sam cvetak zanovetak!!!";
-});
+$stringOne = "first String";
+$stringTwo ="String two";
+//echo $stringOne.$stringTwo;
+$name = "Love ya";
+//echo "hey, im angel of the death ". $name;
+//echo "hey, my name is $name and I'm angel of the death";
+//echo $name[0];
+//echo strlen($name);
+//echo strtoupper($name);
+// echo strtolower($name);
+// echo str_replace("m", "w", $name);
 
-Flight::start();
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
