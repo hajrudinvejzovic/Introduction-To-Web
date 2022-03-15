02969f452d23f3d10cadbd9ec498458572602eb3
hajrudinvejzovic <hajrudin.vejzovic@stu.ibu.edu.ba>
2022-03-15 19:49:56 +0100

    initial commit

--- Index.php
+++ Index.php
@@ -4,6 +4,8 @@ $stringOne = "first String";
 $stringTwo ="String two";
 //echo $stringOne.$stringTwo;
 $name = "Love ya";
+$int= 5.19;
+//echo $name;
 //echo "hey, im angel of the death ". $name;
 //echo "hey, my name is $name and I'm angel of the death";
 //echo $name[0];
@@ -11,6 +13,15 @@ $name = "Love ya";
 //echo strtoupper($name);
 // echo strtolower($name);
 // echo str_replace("m", "w", $name);
+// floor($int); Zaokruzuje cijeli broj i pravi integer.
+// ceil($int); radi isto samo zaokruzuje do gornjeg najblizeg
+
+
+//Array
+//$newarray = [shown, known, stone]; This is indexed array.
+// $associativearray = array("people", "pomoragnge"); This is asssociative array.
+$arrayofintegers = [20,30,40,50];
+print_r(arrayofintegers);
 
 ?>
 <!DOCTYPE html>
