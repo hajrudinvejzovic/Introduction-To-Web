d187901e4321796243c0b4ca4f6ef98fa07f869b
hajrudinvejzovic <hajrudin.vejzovic@stu.ibu.edu.ba>
2022-03-16 09:06:42 +0100

    tesst

--- Index.php
+++ Index.php
@@ -1,36 +1,4 @@
 <?php
 
-$stringOne = "first String";
-$stringTwo ="String two";
-//echo $stringOne.$stringTwo;
-$name = "Love ya";
-$int= 5.19;
-//echo $name;
-//echo "hey, im angel of the death ". $name;
-//echo "hey, my name is $name and I'm angel of the death";
-//echo $name[0];
-//echo strlen($name);
-//echo strtoupper($name);
-// echo strtolower($name);
-// echo str_replace("m", "w", $name);
-// floor($int); Zaokruzuje cijeli broj i pravi integer.
-// ceil($int); radi isto samo zaokruzuje do gornjeg najblizeg
-
-
-//Array
-//$newarray = [shown, known, stone]; This is indexed array.
-// $associativearray = array("people", "pomoragnge"); This is asssociative array.
-$arrayofintegers = [20,30,40,50];
-print_r(arrayofintegers);
-
+echo "First lesson but Heroku doesnt work";
 ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html>
